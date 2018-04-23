3767731f0ce54c7b2c7e753a89c335f3f7b9fa9e
awsalcedo <alex_wss78@hotmail.com>
2018-04-22 23:17:55 -0500

    Configuración de Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAnGA4SuYFTq1PaUjGimllblTvd4RCrsf8",
+    authDomain: "loginapp-bb377.firebaseapp.com",
+    databaseURL: "https://loginapp-bb377.firebaseio.com",
+    projectId: "loginapp-bb377",
+    storageBucket: "",
+    messagingSenderId: "568347751153"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAnGA4SuYFTq1PaUjGimllblTvd4RCrsf8",
+    authDomain: "loginapp-bb377.firebaseapp.com",
+    databaseURL: "https://loginapp-bb377.firebaseio.com",
+    projectId: "loginapp-bb377",
+    storageBucket: "",
+    messagingSenderId: "568347751153"
+  }
 };
