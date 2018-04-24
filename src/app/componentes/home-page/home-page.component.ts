a2fd8ead626264c41686b451b8ec3ed87b862ec6
awsalcedo <alex_wss78@hotmail.com>
2018-04-23 22:03:14 -0500

    Se añade login y logout

--- src/app/componentes/home-page/home-page.component.ts
+++ src/app/componentes/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../servicios/auth.service';
+
+
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit() {
+    
   }
+
+  
 
 }
--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../servicios/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  public email:string;
+  public password:string;
+
+  constructor(private authService:AuthService, private router:Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmitLogin(){
+    this.authService.loginEmail(this.email,this.password)
+    .then((respuesta)=>{
+      //Redireccionamos a la pagina privada
+      this.router.navigate(['/privado']);
+    }).catch((error)=>{
+      console.log(error);
+      this.router.navigate(['/login']);
+    });
   }
 
 }
